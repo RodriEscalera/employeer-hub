e7eb83d388e10d15eaa407dae5d88eda6ecd31aa
RodriEscalera <roescal347@gmail.com>
2024-03-12 17:15:41 -0300

    add user crud routes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,32 @@
 import { Router } from "express";
+import { AuthMiddleware } from "../middlewares/auth.middleware";
+import { UserController } from "../controllers/user.controller";
 
 const router = Router();
+
+router.get(
+  "/",
+  AuthMiddleware.validateUser,
+  AuthMiddleware.checkAdmin,
+  UserController.getUsers
+);
+router.post(
+  "/",
+  AuthMiddleware.validateUser,
+  AuthMiddleware.checkAdmin,
+  UserController.createUser
+);
+router.put(
+  "/update/:_id",
+  AuthMiddleware.validateUser,
+  AuthMiddleware.checkAdmin,
+  UserController.updateUser
+);
+router.delete(
+  "/delete/:_id",
+  AuthMiddleware.validateUser,
+  AuthMiddleware.checkAdmin,
+  UserController.deleteUser
+);
 
 export { router as userRoutes };
