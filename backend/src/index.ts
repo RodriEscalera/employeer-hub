8c6d96a727ebdfc2e4039097c62db056a628ae23
RodriEscalera <roescal347@gmail.com>
2024-03-12 03:21:29 -0300

    add db config and create users model

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,27 @@
 import express, { Express, json, urlencoded } from "express";
 import morgan from "morgan";
 import cors from "cors";
+import syncDB from "./config/db";
 import { envs } from "./config/env/env.config";
+import { User } from "./models/Users/Users.model";
+
 const { PORT, CLIENT_HOST } = envs;
 const app: Express = express();
 const options: cors.CorsOptions = {
   origin: [CLIENT_HOST],
 };
 
+console.log(User);
+
 app.use(morgan("dev"));
 app.use(cors(options));
 app.use(json());
 app.use(urlencoded({ extended: false }));
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+
+(async () => {
+  await syncDB();
+  app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Listening on port ${PORT}`);
+  });
+})();
